0af91d963d3a8b42251c56cff9b1007260d7f471
Shih-Chun You <scy27@cl.cam.ac.uk>
2024-07-22 21:53:49 +0100

    download results

---
+++ project_SHA3-32bit/0001_reference/Code_reference/download.sh
@@ -0,0 +1,3 @@
+DIR='https://www.cl.cam.ac.uk/research/security/datasets/sha3-32bit/Data/0001_reference/Code_reference/'
+wget ${DIR}ref_trace.npy
+wget ${DIR}corrcoef.npy
--- project_SHA3-32bit/0002_detection/Code_preprocessing/clean.sh
+++ project_SHA3-32bit/0002_detection/Code_preprocessing/clean.sh
@@ -1,1 +1,1 @@
-rm -r Raw_*/ Corrcoefs* data_raw_* __pycache__/ check_report.txt
+rm -r ../Processed_HDF5/ Raw_*/ Corrcoefs* data_raw_* __pycache__/ check_report.txt

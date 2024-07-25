a4d22bfeb9cee57291787762df9b52ea3ce5ccf7
Shih-Chun You <scy27@cl.cam.ac.uk>
2024-07-25 15:58:22 +0100

    add download scripts

---
+++ project_SHA3-32bit/0011_test_SHAKE128/SHAKE128_I01/Code_preprocessing/download.sh
@@ -0,0 +1,10 @@
+DIR='https://www.cl.cam.ac.uk/research/security/datasets/sha3-32bit/Data/0011_test_SHAKE128/SHAKE128_I01/'
+mkdir ../Processed_HDF5/
+for part in $(seq -f "%02g" 0 0)
+  do
+    wget ${DIR}Processed_HDF5/Invocation_${part}.hdf5 -P ../Processed_HDF5/
+  done
+wget ${DIR}Code_preprocessing/data_raw_in.zip
+wget ${DIR}Code_preprocessing/data_raw_out.zip
+wget ${DIR}Code_preprocessing/Corrcoefs.zip
+wget ${DIR}Code_preprocessing/check_report.txt
